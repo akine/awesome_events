d854f81abc9366a7a0859808d168357b2a774abb
akine <weezer.and.you@gmail.com>
2020-10-23 22:09:42 +0900

    add: eventstest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,22 @@
 ENV['RAILS_ENV'] ||= 'test'
+
+# SimpleCov
+# require 'simplecov'
+# SimpleCov.start 'rails'
+
 require_relative '../config/environment'
+require_relative 'sign_in_helper'
 require 'rails/test_help'
+require 'minitest/mock'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
-  # Add more helper methods to be used by all tests here...
+  parallelize_setup do |worker|
+    # For Searchkick
+    Searchkick.index_suffix = worker
+    Event.reindex
+    Searchkick.disable_callbacks
+  end
 end
